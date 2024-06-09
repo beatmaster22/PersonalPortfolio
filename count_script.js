eb4801b266887a01b9851085fca5639c53cfc033
Karol Sojka <148217774+beatmaster22@users.noreply.github.com>
2024-06-09 22:35:03 +0200

    Add files via upload

---
+++ count_script.js
@@ -0,0 +1,11 @@
+var count = 0;
+
+function changeCount(num){
+    count += num;
+    document.getElementById('count').innerHTML = count;
+}
+
+function reset(){
+    count = 0;
+    document.getElementById('count').innerHTML = count;
+}
